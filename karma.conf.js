d3154e91573cca981f75608e3a85be55eb435598
Daniel Brain <chuntheunavoidable@gmail.com>
2017-02-01 16:13:01 -0800

    Add sourcemap support to tests

--- karma.conf.js
+++ karma.conf.js
@@ -93,8 +93,8 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'test/test.js': ['webpack'],
-            'test/windows/**/*.js': ['webpack'],
+            'test/test.js': ['webpack', 'sourcemap'],
+            'test/windows/**/*.js': ['webpack', 'sourcemap'],
             'src/**/*.js': ['coverage',  'sourcemap']
         },
 
