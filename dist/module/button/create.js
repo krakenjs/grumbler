c2d797365d9365b823f1e854f530ad39bfa48bdb
Daniel Brain <daniel@bluesuncorp.co.uk>
2018-11-04 15:41:33 -0800

    Dist

--- dist/module/button/create.js
+++ dist/module/button/create.js
@@ -1,17 +1,7 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.createButton = createButton;
-function createButton(container, text, onClick) {
-
-    var button = document.createElement('button');
-
-    button.innerText = text;
-    button.addEventListener('click', onClick);
-
-    container.appendChild(button);
-
-    return button;
+export function createButton(container, text, onClick) {
+  var button = document.createElement('button');
+  button.innerText = text;
+  button.addEventListener('click', onClick);
+  container.appendChild(button);
+  return button;
 }
--- dist/module/index.js
+++ dist/module/index.js
@@ -1,17 +1,1 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _button = require('./button');
-
-Object.keys(_button).forEach(function (key) {
-  if (key === "default" || key === "__esModule") return;
-  Object.defineProperty(exports, key, {
-    enumerable: true,
-    get: function get() {
-      return _button[key];
-    }
-  });
-});
+export * from './button';
